b108da6cd9bb6123d6b3bc33e4a9a6de404594f7
abhishek-netizen <abhishekwithu101@gmail.com>
2023-04-08 03:22:44 +0530

    adding auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import {register} from "./controllers/auth.js";
 
 /* Configurations when using type module */
 const __filename = fileURLToPath(import.meta.url);
@@ -24,6 +25,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+
 /*  FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -36,8 +38,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-/*  MONGOOSE SETUP */
 
+/* ROUTES WITH POSTS */
+app.post("/auth/register", upload.single("picture"), register);
+
+/*  MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 const URL = process.env.MONGO_URL;
 mongoose
