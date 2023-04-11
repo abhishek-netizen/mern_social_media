121a02ff038ae45d012e3896b027171f3ffeb807
abhishek-netizen <abhishekwithu101@gmail.com>
2023-04-12 01:35:43 +0530

    authorisation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
     /*   if matched lets generate the token */
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password; // a masterpiece
-    res.status(200).json({ token, user });
+    res.status(200).json({ token, user }); // send entire user info to client side
   } catch (error) {
     res.status(500).json({ error: err.message });
   }
